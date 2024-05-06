a63612d756b41a35092103dfc87a18b086a8fffd
Arash Ahmadi <arash.ahmadi@live.com>
2024-05-06 09:38:51 +0400

    Update

---
+++ credentials.h
@@ -0,0 +1,11 @@
+/*  
+ * - Make a copy of this file and rename it to 'credentials.h'.
+ * - fill in the required credentials for WiFi MQTT broker access.
+  */
+
+#define WIFI_SSID "******"
+#define WIFI_PASSWORD "*******"
+#define MQTT_USER "gcty1juz6wej41sym21y" 
+#define MQTT_PASSWORD "4hrk2f89r7kwfcstxqgk"
+
+#define SECRET_PINNUMBER     ""
